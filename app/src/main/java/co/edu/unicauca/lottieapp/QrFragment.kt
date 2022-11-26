5cb7630a1a812d3f6d64cd45b07022b529c83a78
damd2212 <53495267+damd2212@users.noreply.github.com>
2022-11-25 22:24:49 -0500

    scanner falta busqueda de escenario

--- app/src/main/java/co/edu/unicauca/lottieapp/QrFragment.kt
+++ app/src/main/java/co/edu/unicauca/lottieapp/QrFragment.kt
@@ -1,11 +1,16 @@
 package co.edu.unicauca.lottieapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+
 import co.edu.unicauca.lottieapp.databinding.FragmentQrBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 
 class QrFragment : Fragment() {
@@ -23,4 +28,35 @@ class QrFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        initScanner()
+        //binding.btnFragmentQr.setOnClickListener { initScanner() }
+    }
+
+    private fun initScanner() {
+        val integrator = IntentIntegrator.forSupportFragment(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+        integrator.setPrompt("Escanea el codigo QR de la cancha")
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+
+        if (result != null){
+            if (result.contents == null){
+                Toast.makeText(getActivity(), "Escaner cancelado", Toast.LENGTH_SHORT).show();
+                findNavController().navigate(R.id.action_qrFragment_to_locationFragment)
+            }else{
+                Toast.makeText(getActivity(), "Valor escaneado: ${result.contents}", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+
+        }
+
+    }
 }
