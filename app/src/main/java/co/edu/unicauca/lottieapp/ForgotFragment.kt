3003237c8943eda99bdee50fa259ef714d5bfbbe
jmora216 <70669461+jmora216@users.noreply.github.com>
2022-12-05 18:00:21 -0500

    Recuperación de contraseña

--- app/src/main/java/co/edu/unicauca/lottieapp/ForgotFragment.kt
+++ app/src/main/java/co/edu/unicauca/lottieapp/ForgotFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import co.edu.unicauca.lottieapp.databinding.FragmentForgotBinding
+import co.edu.unicauca.lottieapp.repository.UserRepository
 
 class ForgotFragment : Fragment() {
 
     private var _binding: FragmentForgotBinding? = null
 
     private val binding: FragmentForgotBinding get() = _binding!!
+    private val userRepository by lazy { UserRepository() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +30,15 @@ class ForgotFragment : Fragment() {
         super.onStart()
 
         binding.fragmentForgotButton.setOnClickListener {
+            if (!binding.forgotEmail.text.toString().isValidEmail()){
+                binding.forgotEmailLayout.error = getString(R.string.password_error)
 
+            }else{
+                binding.forgotEmailLayout.error = null
+                userRepository.sendResetPassword(binding.forgotEmail.text.toString())
+                Toast.makeText(requireContext(),"se enviaron instrucciones a tu correo para recuperar la clave", Toast.LENGTH_LONG).show()
+                findNavController().popBackStack()
+            }
         }
 
         binding.fragmentForgotLabel2.setOnClickListener {
