f20e1eab785c3ac0354e6b32c92b950b8ff917a5
jmora216 <70669461+jmora216@users.noreply.github.com>
2022-12-19 11:26:09 -0500

    Actualización LoginActivity

--- app/src/main/java/co/edu/unicauca/lottieapp/ui/activities/LoginActivity.kt
+++ app/src/main/java/co/edu/unicauca/lottieapp/ui/activities/LoginActivity.kt
@@ -7,5 +7,20 @@ import co.edu.unicauca.lottieapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    
+    private lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.buttonBackLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
 }
